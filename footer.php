65a8ee10db00dad900f9b106b88e70a6c6a280ee
hawkeye126 <pete.lower@gmail.com>
2014-08-27 16:05:38 -0700

    background image style updates, moving footer into page

--- footer.php
+++ footer.php
@@ -9,17 +9,17 @@
  * @since Twenty Twelve 1.0
  */
 ?>
+	
+			<?php if(!is_front_page()) : ?>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<?php endif; ?>
+		<footer id="colophon" role="contentinfo">
+			<div class="site-info">
+				<h4>Stamats Health Care Marketing &copy;<?php echo date("Y"); ?></h4>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
 	</div><!-- #main .wrapper -->
 </div><!-- #page -->
-	<?php if(!is_front_page()) : ?>
-		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-	<?php endif; ?>
-<footer id="colophon" role="contentinfo">
-	<div class="site-info">
-		<h4>Stamats Health Care Marketing &copy;<?php echo date("Y"); ?></h4>
-	</div><!-- .site-info -->
-</footer><!-- #colophon -->
-
 <?php wp_footer(); 
 
 /*
