c12721a72f5a9d146b58c48eac1ad4f7a61957b6
hawkeye126 <pete.lower@gmail.com>
2014-08-29 14:07:11 -0700

    updating header viewport

--- header.php
+++ header.php
@@ -18,9 +18,13 @@
 <!--[if !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
-<head>
+<head>	
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<!-- <meta name="viewport" content="width=device-width" /> -->
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> -->
+<!-- <meta name="viewport" content="initial-scale=1, maximum-scale=1"> -->
+<!-- <meta name="viewport" content="initial-scale=1"> -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
