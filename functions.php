d9cdffa577cb40a599a0828771dc1233fa3cd255
hawkeye126 <pete.lower@gmail.com>
2014-09-17 09:30:44 -0700

    add function call to wp to create new footer menu

--- functions.php
+++ functions.php
@@ -9,3 +9,8 @@ function lowermedia_enqueue_parent_style() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'lowermedia_enqueue_parent_style' );
+
+function lowermedia_register_my_menu() {
+  register_nav_menu('footer-social-menu',__( 'Footer Social Menu' ));
+}
+add_action( 'init', 'lowermedia_register_my_menu' );
