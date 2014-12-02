1cdda36786eafac484d82f64aae472eff40a3b7f
hawkeye126 <pete.lower@gmail.com>
2014-12-02 15:53:58 -0800

    add ux compatability

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
 <!--<![endif]-->
 <head>	
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <!-- <meta name="viewport" content="width=device-width" /> -->
 <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> -->
 <!-- <meta name="viewport" content="initial-scale=1, maximum-scale=1"> -->
