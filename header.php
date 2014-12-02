e39003a2893a90c10e882e0bdfd34c258a965e9a
hawkeye126 <pete.lower@gmail.com>
2014-12-02 15:54:48 -0800

    remove viewport comments

--- header.php
+++ header.php
@@ -21,10 +21,6 @@
 <head>	
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<!-- <meta name="viewport" content="width=device-width" /> -->
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> -->
-<!-- <meta name="viewport" content="initial-scale=1, maximum-scale=1"> -->
-<!-- <meta name="viewport" content="initial-scale=1"> -->
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
